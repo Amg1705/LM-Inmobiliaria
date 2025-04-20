bcd1e40e1299c98699f2e348d27d3ad0839ebcab
Amg1705 <alejandro.moralesgimeno@cieep.com>
2025-04-20 22:36:10 +0200

    ..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,8 @@ import { IniciosesionComponent } from './paginas/iniciarsesion/iniciarsesion.com
 import { RegistroComponent } from './paginas/registro/registro.component';
 import { SobrenosotrosComponent } from './paginas/sobrenosotros/sobrenosotros.component';
 import { CatalogoComponent } from './paginas/catalogo/catalogo.component';
-import { DetallePropiedadComponent } from './paginas/detalle-propiedad/detalle-propiedad.component';
 import { PagoComponent } from './paginas/pago/pago.component';
-//import { NotFoundComponent } from './paginas/not-found/not-found.component';
+import { NotFoundComponent } from './paginas/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
@@ -15,13 +14,12 @@ export const routes: Routes = [
   { path: 'catalogo', component: CatalogoComponent },
   { path: 'pago', component: PagoComponent },
   { path: 'registro', component: RegistroComponent },
-  { path: 'catalogo/:id', component: DetallePropiedadComponent },
-  //{ path: '**', component: NotFoundComponent },
   {
     path: 'detalle/:id',
     loadComponent: () =>
       import('./paginas/detalle-propiedad/detalle-propiedad.component').then(
         m => m.DetallePropiedadComponent
       )
-  }
+  },
+  { path: '**', component: NotFoundComponent }
 ];
