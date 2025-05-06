76c4e5c53bcaf367b1e6c28b2043505570ba10a5
Alex <alejandro.moralesgimeno@cieep.com>
2025-05-06 18:27:13 +0200

    ultimooo1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegistroComponent } from './paginas/registro/registro.component';
 import { SobrenosotrosComponent } from './paginas/sobrenosotros/sobrenosotros.component';
 import { CatalogoComponent } from './paginas/catalogo/catalogo.component';
 import { PagoComponent } from './paginas/pago/pago.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
 import { NotFoundComponent } from './paginas/not-found/not-found.component';
 
 export const routes: Routes = [
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: 'sobre-nosotros', component: SobrenosotrosComponent },
   { path: 'catalogo', component: CatalogoComponent },
   { path: 'pago', component: PagoComponent },
+  {path: 'contacto', component: ContactoComponent},
   { path: 'registro', component: RegistroComponent },
   {
     path: 'detalle/:id',
--- src/app/componentes/piedepagina/piedepagina.component.ts
+++ src/app/componentes/piedepagina/piedepagina.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-piedepagina',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './piedepagina.component.html',
   styleUrl: './piedepagina.component.css'
 })
